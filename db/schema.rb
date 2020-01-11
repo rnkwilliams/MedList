fdb1b1c743d64c50dfcd2ecf779d1b91e9e451ec
github username <github email address>
2020-01-11 10:56:21 -0600

    add category resource migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_11_165048) do
+ActiveRecord::Schema.define(version: 2020_01_11_165520) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "conditions", force: :cascade do |t|
     t.string "name"
