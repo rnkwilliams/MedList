e61040cc5a84f071310a164159f5e4b82f4034ef
github username <github email address>
2020-01-13 12:23:48 -0600

    add signup/login routes, views, controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  root 'static#home'
+
+  get '/signup', to: 'signup#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
   resources :categories
   resources :conditions
   resources :users
