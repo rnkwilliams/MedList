3df7270140e465b5dde7a37f9e1ad12532ed0a1c
github username <github email address>
2020-01-15 11:18:01 -0600

    add medication controller actions

--- app/controllers/medications_controller.rb
+++ app/controllers/medications_controller.rb
@@ -4,9 +4,18 @@ class MedicationsController < ApplicationController
         @medication.build_category
     end
 
+    def create
+        @medication = Medication.new(medication_params)
+        if @medication.save
+            redirect_to medication_path(@medication)
+        else
+            render :new
+        end
+    end
+
     private
 
     def medication_params
-    
+        params.require(:medication).permit(:name, :dosage, :units, :frequency,           :times_per_day, :start_date, :category_id, category_attributes: [:name])
     end
 end
