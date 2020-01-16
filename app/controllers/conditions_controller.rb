c40b33e843f811ec6eb6fd2104f631cd7eacc4e9
github username <github email address>
2020-01-16 17:11:06 -0600

    add code for condition controller, model, views

--- app/controllers/conditions_controller.rb
+++ app/controllers/conditions_controller.rb
@@ -4,6 +4,25 @@ class ConditionsController < ApplicationController
         @condition = @medication.conditions.build
     end
 
+    def create
+        @condition = Condition.new(condition_params)
+        if @condition.save
+            redirect_to condition_path(@condition)
+        else
+            render :new
+        end
+    end
+
+    def show
+        @condition = Condition.find_by_id()
+    end
+
     def index
+    end
+
+    private
+
+    def condition_params
+       params.require(:condition).permit(:name, :notes, :medication_id) 
     end
 end
--- app/models/condition.rb
+++ app/models/condition.rb
@@ -1,4 +1,6 @@
 class Condition < ApplicationRecord
   belongs_to :user
   belongs_to :medication
+
+  validates :name, presence: true
 end
