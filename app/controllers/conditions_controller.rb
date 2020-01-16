9faa35869dec0576fe4dfd44c2eb90086ec2f46b
github username <github email address>
2020-01-16 15:11:24 -0600

    add condition controller actions and views

--- app/controllers/conditions_controller.rb
+++ app/controllers/conditions_controller.rb
@@ -1,5 +1,7 @@
 class ConditionsController < ApplicationController
     def new
+        @medication = Medication.find_by_id(params[:medication_id])
+        @condition = @medication.conditions.build
     end
 
     def index
