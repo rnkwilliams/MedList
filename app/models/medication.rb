a105bd2dbd03e45e61b8e133dec5d2f48d5ee6fc
github username <github email address>
2020-01-20 14:42:01 -0600

    add medication validations, updated views

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -4,5 +4,20 @@ class Medication < ApplicationRecord
   has_many :conditions
   has_many :users, through: :conditions
 
+  validates :name, presence: true
+  validates :dosage, presence: true
+  validates :units, presence: true
+  validates :frequency, presence: true
+  validates :times_per_day, presence: true
+  validates :start_date, presence: true
+  validate :not_a_duplicate
+
   accepts_nested_attributes_for :category
+
+  def not_a_duplicate
+    if Medication.find_by(name: name, category_id: category_id)
+      errors.add(:name, "has already been added for that category")
+    end
+  end
+  
 end
